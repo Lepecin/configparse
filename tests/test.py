6a6b036f2c88eaf7be4c2006e7c8eeb4eb3df8a9
Ivan Silajev <evantrulife@gmail.com>
2023-09-30 13:32:20 +0100

    Finalise test file

--- tests/test.py
+++ tests/test.py
@@ -1,20 +1,20 @@
-from lepecinconfigparse import ConfigManager, ConfigLoader
+from lepecinconfigparse import ConfigManager, JsonConfigLoader
 
 import pathlib
 
-
+path_config = (pathlib.Path(__file__).parent / "config.json").__str__()
 path_internal = (pathlib.Path(__file__).parent / "internal-config.json").__str__()
 path_constants = (pathlib.Path(__file__).parent / "constants.py").__str__()
 
 
 manager = ConfigManager(
     path_internal,
-    ConfigLoader(),
+    JsonConfigLoader(),
 )
 
 
 if __name__ == "__main__":
-    manager.set_internal_config()
+    manager.set_internal_config(path_config)
     manager.save_internal_constants(path_constants)
     globals().update(manager.load_internal_config())
 
