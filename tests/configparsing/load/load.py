8851ca6d6d9cb970a802c70bcae73a01f7b4d609
Lepecin <evantrulife@gmail.com>
2023-10-14 20:03:28 +0100

    Create loading files

---
+++ tests/configparsing/load/load.py
@@ -0,0 +1,15 @@
+import pathlib
+
+from lepconfigparse import ConfigManager, JsonConfigLoader
+
+path_internal = (pathlib.Path(__file__).parent / "config.json").__str__()
+
+manager = ConfigManager(
+    path_internal,
+    JsonConfigLoader(),
+)
+
+config = manager.load_internal_config()
+
+globals().update(config)
+__all__ = list(config.keys())
