c310497b8ec6e0af06f15bc8851800c378082e7b
Lepecin <evantrulife@gmail.com>
2023-10-14 18:16:26 +0100

    Add config loader abstraction

---
+++ src/lepconfigparse/loaders/abstract.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from typing_extensions import Self
+
+
+from .types import Config
+
+
+class ConfigLoaderAbstract(ABC):
+    """
+    Abstract class for loading and saving config style
+    objects into a Python environment.
+
+    Has two methods, a getter and a setter for modifying
+    the state of the machine file system.
+    - load (getter)
+    - save (setter)
+    """
+
+    @abstractmethod
+    def load(self: Self, path: str) -> Config:
+        pass
+
+    @abstractmethod
+    def save(self: Self, path: str, config: Config):
+        pass
